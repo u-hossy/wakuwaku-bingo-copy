36d28c530836046c49c462f5c32773143f1fa820
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-10-16 20:51:02 +0900

    Authの追加

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,9 +1,12 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    databaseURL: "https://wkwk-bingo-default-rtdb.firebaseio.com/"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  databaseURL: "https://wkwk-bingo-default-rtdb.firebaseio.com/",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
