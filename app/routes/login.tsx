64f435edda31b739529d47d32a853a6e8da8ce1b
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-10-17 11:54:25 +0900

    問題ありそうだけど認証を作成

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,24 +1,40 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Form, } from "@remix-run/react";
 import { Button, Input } from "@headlessui/react";
+import { ActionFunctionArgs, redirectDocument } from "@remix-run/cloudflare";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const auth = getAuth();
+    const isOK: boolean = await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            return true;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error(errorCode, errorMessage);
+            return false;
+            // return json({ errorCode: errorCode, errorMessage: errorMessage }, { status: 400 });
+        });
+
+    if (isOK == true) {
+        return redirectDocument("/admin");
+    } else {
+        return redirectDocument("/login");
+    }
+}
 
 export default function AdminLogin() {
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("submitted");
-        console.log(event);
-        const formData = new FormData(event.currentTarget);
-        const email = formData.get("email") as string;
-        console.log(email);
-    }
-
-
     return (
         <>
-            <Form onSubmit={handleSubmit}>
+            <Form method="POST">
                 <label htmlFor="email">email</label>
-                <Input name="email" type="email" className="mt-3 block w-full rounded-lg border bg-white/5 py-1.5 px-3 text-sm/6"></Input>
+                <Input name="email" type="email" className="mb-3 block w-full rounded-lg border bg-white/5 py-1.5 px-3 text-sm/6"></Input>
                 <label htmlFor="password">password</label>
-                <Input name="password" type="password" className="mt-3 block w-full rounded-lg border bg-white/5 py-1.5 px-3 text-sm/6"></Input>
+                <Input name="password" type="password" className="mb-3 block w-full rounded-lg border bg-white/5 py-1.5 px-3 text-sm/6"></Input>
                 <Button type="submit" className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
                     Sign In
                 </Button>
