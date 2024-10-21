345bef99d5e3f36a8e46aa6b447336225a5f487d
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-10-21 19:09:27 +0900

    WIP

--- app/components/Numbers.tsx
+++ app/components/Numbers.tsx
@@ -5,30 +5,6 @@ import { db } from "firebase/config";
 export default function Numbers() {
     const [numbers, setNumbers] = useState<{ "order": number }[]>([]);
 
-    useEffect(() => {
-        // const getData = () => {
-        //     const dataRef = ref(db, "number/");
-        //     onValue(dataRef, (snapshot) => {
-        //         const data = snapshot.val();
-        //         console.log(data);
-        //         setNumbers(data);
-        //         console.log("Data received");
-        //     });
-        // }
-        // getData();
-        const dataRef = ref(db, "number/");
-        return onValue(dataRef, (snapshot) => {
-            const data = snapshot.val();
-            if (snapshot.exists()) {
-                (data).map((number) => {
-                    setNumbers((prev) => {
-                        return [...prev, { order: number as number }]
-                    })
-                })
-            }
-        })
-    });
-
     return (
         <>
             <div>Numbers</div>
