268347a69364016db44b2b51ab965715af2bb9ad
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-10-21 19:09:42 +0900

    WIP

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,6 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 import { useState } from "react";
 import Numbers from "~/components/Numbers";
 import Prizes from "~/components/Prizes";
-import { NowPage } from "../types/NowPage";
 
 export const meta: MetaFunction = () => {
   return [
