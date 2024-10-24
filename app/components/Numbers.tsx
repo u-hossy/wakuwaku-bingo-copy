838842aa6fb83b55a63a097761124b31185dc206
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-10-24 12:51:22 +0900

    無限ループ。。。

--- app/components/Numbers.tsx
+++ app/components/Numbers.tsx
@@ -5,6 +5,18 @@ import { db } from "firebase/config";
 export default function Numbers() {
     const [numbers, setNumbers] = useState<{ "order": number }[]>([]);
 
+    useEffect(() => {
+        const numberRef = ref(db, "number/");
+        onValue(numberRef, (snapshot) => {
+            const data = snapshot.val();
+            console.log(data);
+            if (data) {
+                setNumbers(Object.values(data));
+            }
+        });
+    }, [numbers]);
+
+
     return (
         <>
             <div>Numbers</div>
