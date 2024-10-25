033daa355c2734ede406d80bf300a1b1dac92030
hossy <hossy@windows.example.com>
2024-10-25 17:22:53 +0900

    RealtimeDatabaseの型を扱うための型定義を追加

---
+++ app/types/dataTypes.ts
@@ -0,0 +1,13 @@
+// ビンゴの各番号を保存するところ
+type BingoNumber = { name: number; order: number };
+
+// ビンゴの景品を保存するところ
+type Prize = { name: string; amount: number };
+
+// プロジェクターの表示モードを保存するところ
+type ProjectorMode = "latest" | "history";
+
+// ゲームの開始状態を保存するところ
+type IsStarted = boolean;
+
+export type { BingoNumber, Prize, ProjectorMode, IsStarted };
