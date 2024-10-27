fa9fc18bad66abfdab553deb2e5cc5b3c5fb9cac
tomoka <tomoka@example.com>
2024-10-27 13:27:20 +0900

    ビンゴの番号表示するコンポーネントを作成

--- app/components/NumberBox.tsx
+++ app/components/NumberBox.tsx
@@ -1,1 +1,8 @@
-export function NumberBox
+export default function NumberBox({ calledNumber }: { calledNumber: number, size: number }) {
+    return (
+        <div className="relative h-8 w-8 flex item-center justify-center">
+            <img src="./number-box.png" alt={"No. " + calledNumber} className="absolute inset-0 w-full h-full"/>
+            <span className="text-black text-2xl font-bold">{calledNumber}</span>
+        </div>
+    )
+}
