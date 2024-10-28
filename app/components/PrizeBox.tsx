49ed147475d3c2c9f5194f0fc70fe933f04fa730
haruka <haruka@example.com>
2024-10-28 19:00:02 +0900

    BOXの作成

--- app/components/PrizeBox.tsx
+++ app/components/PrizeBox.tsx
@@ -1,1 +1,19 @@
-export function PrizeBox
+export default function PrizeBox({image,name,amount}:{image: string, name: string, amount: number}) {
+  return (
+    <div style={styles.container}>
+        <img src={image} alt={name}/>
+        <p>{name}</p>
+	      <p>残り{amount}個</p>
+    </div>
+  );
+}
+
+const styles: { container: React.CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: 'center',
+    padding: '20px',
+  },
+};
+
