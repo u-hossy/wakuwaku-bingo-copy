c7e632afbd37fec6428d58627ded07c6b1b6c2c0
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-11-04 15:26:23 +0900

    カスタムフック名を変更

--- app/components/Numbers.tsx
+++ app/components/Numbers.tsx
@@ -1,12 +1,7 @@
-// import { useEffect, useState } from "react";
-// import { ref, onValue } from "firebase/database";
-// import { realtimeDatabase } from "firebase/config";
-// import { BingoNumber } from "~/types/dataTypes";
-import { useFetchNumber } from "~/hooks/useFetchData";
+import { useFetchNumber } from "~/hooks/useRealtimeDatabase";
 
 export default function Numbers() {
     const [numbers] = useFetchNumber();
-
     return (
         <>
             <div>Numbers</div>
