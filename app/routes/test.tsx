b6f2a0ea55d14b16963ec9fd0ad5c410a7f2acc3
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-11-10 00:05:46 +0900

    テスト用のページを削除

--- app/routes/test.tsx
+++
@@ -1,9 +0,0 @@
-import AdminNumberList from "~/components/AdminNumberList"
-
-export default function test() {
-    return (
-        <div>
-            <AdminNumberList />
-        </div>
-    )
-}
