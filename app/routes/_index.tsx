833c4fbf5ceb9981ffa1006af610f5d51842511e
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-11-12 12:03:55 +0900

    import文の整理

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,11 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Numbers from "~/components/Numbers";
-import Prizes from "~/components/Prizes";
-import "../index.css";
 import PrizeBox from "~/components/PrizeBox";
+import Prizes from "~/components/Prizes";
 
 import type { MetaFunction } from "@remix-run/cloudflare";
+
 import "../styles/index.css";
 
 export const meta: MetaFunction = () => {
@@ -22,11 +21,11 @@ export default function Index() {
   return (
     <>
       <div className="tab-container">
-        <button className={nowPage === "numbers" ? "active tab-button" : "inactive tab-button" } onClick={() => setNowPage("numbers")}>NUMBERS</button>
-        <button className={nowPage === "prizes" ? "active tab-button" : "inactive tab-button" } onClick={() => setNowPage("prizes")}>PRIZES</button>
-        <PrizeBox image="img/pizza_toast.png" name="aaa"  amount={3} isSold={true}/>
+        <button className={nowPage === "numbers" ? "active tab-button" : "inactive tab-button"} onClick={() => setNowPage("numbers")}>NUMBERS</button>
+        <button className={nowPage === "prizes" ? "active tab-button" : "inactive tab-button"} onClick={() => setNowPage("prizes")}>PRIZES</button>
+        <PrizeBox image="img/pizza_toast.png" name="aaa" amount={3} isSold={true} />
       </div >
-      
+
       {nowPage === "numbers" ? <Numbers /> : <Prizes />}
     </>
   );
