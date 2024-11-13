997f0c9639ec08b792a29f83919247310054a763
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-11-13 23:03:31 +0900

    3つのサイズを用意した

--- app/components/NumberBox.tsx
+++ app/components/NumberBox.tsx
@@ -1,38 +1,92 @@
 export default function NumberBox({ calledNumber, size }: { calledNumber: number, size: "large" | "medium" | "small" }) {
-    const styles: { container: React.CSSProperties, image: React.CSSProperties, number: React.CSSProperties } = {
+    const styles_small: { container: React.CSSProperties, image: React.CSSProperties, number: React.CSSProperties } = {
         container: {
             position: "relative",
-            width: "11rem",
-            height: "11rem",
+            width: "23vw",
+            height: "23vw",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
         },
         image: {
             position: "absolute",
-            width: "11rem",
-            height: "11rem",
+            width: "23vw",
+            height: "auto",
         },
         number: {
             position: "absolute",
-            top: "3.9rem",
-            fontSize: "3em",
+            top: "8.75vw",
+            fontSize: "6vw",
+            fontWeight: "bold",
+        },
+    };
+    
+    const styles_medium: { container: React.CSSProperties, image: React.CSSProperties, number: React.CSSProperties } = {
+        container: {
+            position: "relative",
+            width: "19vw",
+            height: "19vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        image: {
+            position: "absolute",
+            width: "19vw",
+            height: "auto",
+        },
+        number: {
+            position: "absolute",
+            top: "9.5vw",
+            fontSize: "5vw",
             fontWeight: "bold",
         },
     };
 
+    const styles_large: { container: React.CSSProperties, image: React.CSSProperties, number: React.CSSProperties } = {
+        container: {
+            position: "relative",
+            width: "90vh",
+            height: "90vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        image: {
+            position: "absolute",
+            width: "90vh",
+            height: "auto",
+        },
+        number: {
+            position: "absolute",
+            top: "29vh",
+            fontSize: "29vh",
+            fontWeight: "bold",
+        },
+    };
+
+
     switch (size) {
         case "small":
-            console.log("Small size");
-            break;
+            return (
+                <div style={styles_small.container}>
+                    <img src="./number-box.png" alt={"No. " + calledNumber} style={styles_small.image} />
+                    <span style={styles_small.number}>{calledNumber}</span>
+                </div>
+            );
         case "medium":
             return (
-                <div style={styles.container}>
-                    <img src="./number-box.png" alt={"No. " + calledNumber} style={styles.image} />
-                    <span style={styles.number}>{calledNumber}</span>
+                <div style={styles_medium.container}>
+                    <img src="./number-box.png" alt={"No. " + calledNumber} style={styles_medium.image} />
+                    <span style={styles_medium.number}>{calledNumber}</span>
                 </div>
             );
         case "large":
-            console.log("Large size");
+            return (
+                <div style={styles_large.container}>
+                    <img src="./number-box.png" alt={"No. " + calledNumber} style={styles_large.image} />
+                    <span style={styles_large.number}>{calledNumber}</span>
+                </div>
+            );
     }
 }
