12231dbb0f7ab9fe202453cf7d9558c5ac9c2b45
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-11-17 11:05:45 +0900

    `transition`

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center transition">
       <p className="text-3xl md:text-5xl font-bold my-1 md:my-8 text-slate-50">
         <span className="inline-block">2024年度理大祭</span>{" "}
         <span className="inline-block">前夜祭ビンゴ</span>
